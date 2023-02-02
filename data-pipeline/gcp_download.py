8c08426c3ddd9240e20de6348fcf5132ac273d6a
Areel Khan <you@example.com>
2023-02-02 01:10:42 -0500

    Created datapipeline class skeleton and low fidelity wireframe

--- data-pipeline/gcp_download.py
+++ data-pipeline/gcp_download.py
@@ -1,58 +1,158 @@
-import os
+import json
 
-import zipfile
-import xarray as xr
-import imagecodecs
-import jpeg_xl_float_with_nans
+from os import getcwd
+
 from datetime import datetime
 from dateutil.relativedelta import *
+
 import matplotlib.pyplot as plt
 from huggingface_hub import hf_hub_download
+import xarray as xr
 
-START_DATE = datetime.strptime("January 1, 2022", "%B %d, %Y")
-END_DATE = datetime.strptime("January 1, 2022", "%B %d, %Y")
-FEATURES = []
-INTERVAL = relativedelta(days=1)
-
-##
+# PROCESS
 # 1. Download from HuggingFace
 # 2. Unzip data
 # 3. Preprocess
 # 4. Upload
+# 5. Clean up
+
+
+class GCPPipeline:
+    def __init__(self) -> None:
+        self.config = config
+
+    def unzip(source: str, dest: str) -> None:
+        """
+        Unzips all files from the source dir and extracts them to the dest dir
+
+        Args:
+            source: source file path
+            dest: destination file path
+        
+        Returns:
+            None
+        """
+        pass
+
+    def gcp_upload(source: str, dest: str) -> None:
+        """
+        Upload data from the source dir to a GCP bucket at dest
+
+        Args:
+            source: source file path
+            dest: GCP bucket file path
+        
+        Returns:
+            None
+        """
+        pass
+
+    def cleanup(file: str) -> None:
+        """
+        Deletes the all contents of file
+        """
+        pass
+
+
+class SatelliteUpload(GCPPipeline):
+    def __init__(self, config: dict) -> None:
+        super().__init__(config)
+
+    def download(self) -> str:
+        print("Downloading Data")
+        try:
+            for chunk in range(11, self.config['chunk_count'] + 1):
+                file = f"data/{self.config['year']}/hrv/{self.config['year']}_0000{str(chunk).zfill(2)}-of-0000{self.config['chunk_count']}.zarr.zip"
+                path = hf_hub_download(
+                    repo_id=self.config['hf_repo_id'],
+                    filename=file,
+                    repo_type=self.config['hf_repo_type'],
+                    token=self.config['hf_token'],
+                    cache_dir=getcwd()
+                )
+        except Exception as error:
+            print(error)
+
+        print("Download Complete")
+
+        return 'THIS SHOULD BE THE PATH WHERE THE DATA WAS DOWNLOADED'
+
+    def preprocess(self) -> None:
+        """
+        Preprocesses the data as desired
+        This function should probably take instructions from the download_configurations JSON file about the crop range, date range and features to drop
+        """
+        pass
+
+    def execute(self: str) -> None:
+        self.download()
+        self.unzip()
+        self.prepocess()
+        self.gcp_upload()
+        self.cleanup()
+
+
+class NWPUpload(GCPPipeline):
+    def __init__(self, config: dict) -> None:
+        super().__init__(config)
+
+    def download(self) -> str:
+        START_DATE = datetime.strptime("January 1, 2022", "%B %d, %Y")
+        END_DATE = datetime.strptime("January 1, 2022", "%B %d, %Y")
+        FEATURES = []
+        INTERVAL = relativedelta(days=1)
+
+        date = START_DATE
+        filename = f"data/{'YEAR'}/hrv/{'YEAR'}_000000-of-000056.zarr.zip"
+
+        while date <= END_DATE:
+            # 1. Download Data
+            partition = filename.replace('YEAR', str(date.year)) \
+                                .replace('MONTH', str(date.month).zfill(2)) \
+                                .replace('DAY', str(date.day).zfill(2))
+            print(f"Downloading {partition}...")
+
+            path = hf_hub_download(repo_id="openclimatefix/eumetsat-rss",
+                                    filename=partition,
+                                    repo_type="dataset",
+                                    token='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')
+
+            # 2. Unzip Data
+            data_path = f"./gcp-data/{partition[-30:-4]}"
+            print(f"Unzipping to {data_path}")
+            # with zipfile.ZipFile(path, "r") as zip_ref:
+            #     zip_ref.extractall(data_path)
+
+            # 3. Preprocess data
+            print("Preprocessing...")
+            dataset = xr.open_dataset(data_path, engine='zarr', chunks='auto')
+
+            time_slice = dataset.indexes['time'][0]
+            fig = plt.figure()
+            im = plt.imshow(dataset["data"].sel(time=time_slice))
+
+            date += INTERVAL
+            print(date)
+            break
+
+        print("Done")
+
+    def preprocess(self: str) -> None:
+        """
+        Preprocesses the data as desired
+        This function should probably take instructions from the download_configurations JSON file about the crop range, date range and features to drop
+        """
+        pass
+
+    def execute(self: str) -> None:
+        self.download()
+        self.unzip()
+        self.prepocess()
+        self.gcp_upload()
+        self.cleanup()
 
 if __name__ == '__main__':
-    date = START_DATE
-    filename = f"data/{'YEAR'}/hrv/{'YEAR'}_000000-of-000056.zarr.zip"
-
-    while date <= END_DATE:
-        # 1. Download Data
-        partition = filename.replace('YEAR', str(date.year)) \
-                            .replace('MONTH', str(date.month).zfill(2)) \
-                            .replace('DAY', str(date.day).zfill(2))
-        print(f"Downloading {partition}...")
-
-        path = hf_hub_download(repo_id="openclimatefix/eumetsat-rss",
-                               filename=partition,
-                               repo_type="dataset",
-                               token='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')
-
-        # 2. Unzip Data
-        data_path = f"./gcp-data/{partition[-30:-4]}"
-        print(f"Unzipping to {data_path}")
-        # with zipfile.ZipFile(path, "r") as zip_ref:
-        #     zip_ref.extractall(data_path)
-
-        # 3. Preprocess data
-        print("Preprocessing...")
-        dataset = xr.open_dataset(data_path, engine='zarr', chunks='auto')
-
-        time_slice = dataset.indexes['time'][0]
-        fig = plt.figure()
-        im = plt.imshow(dataset["data"].sel(time=time_slice))
-
-        date += INTERVAL
-        print(date)
-        break
-
-    print("Done")
+    config = json.load(open('datapipeline_config.json'))
+    datapipeline = SatelliteUpload(config=config)
+    datapipeline.execute()
 
