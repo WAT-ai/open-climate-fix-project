e3966870284d6559b4238e3352a47e0dd3fa6af0
Areel Khan <you@example.com>
2023-02-06 20:46:57 -0500

    applied style and documenting changes

--- datapipeline/gcp_datapipelines.py
+++ datapipeline/gcp_datapipelines.py
@@ -17,6 +17,9 @@ class GCPPipeline:
     def __init__(self, config: str) -> None:
         """
         Intialization function for pipeline. Expects a path to JSON configuration file.
+
+        Args:
+            config: path to JSON config object
         """
         self.config = json.load(open(config))
 
@@ -50,8 +53,104 @@ class GCPPipeline:
     def teardown(self, filepath: str) -> None:
         """
         Deletes the directory at filepath and all of its contents
+
+        Args:
+            filepath: location of directory to delete
         """
         pass
+
+
+
+
+class NWPPipeline(GCPPipeline):
+    def __init__(self, config: str) -> None:
+        super().__init__(config)
+
+    def download(self, filepath: str) -> str:
+        """
+        Downloads data from "filepath" location from HugginFace and 
+        returns the location of the downloaded data. If the filepath 
+        is not found, it logs the error in a log file.
+
+        Args:
+            filepath: HuggingFace data location
+        
+        Returns:
+            returns filepath of downloaded data
+        """
+        print(f'\nDownloading: {filepath}...')
+        try:
+            download_path = hf_hub_download(
+                repo_id='openclimatefix/eumetsat-rss',
+                filename=filepath,
+                repo_type=self.config['hf_repo_type'],
+                token=self.config['hf_token'],
+                cache_dir=getcwd() + '/cache'
+            )
+            return download_path
+        except Exception as error:
+            log_file = open(self.config['error_log_path'] + 'error_logs.txt', 'a')
+            log_file.write(str(error))
+            print(error)
+
+    def preprocess(self, filepath: str) -> None:
+        """
+        Preprocesses the zarr file at filepath according to configuration parameters
+
+        Args:
+            filepath: location of zarr path
+        """
+        pass
+
+    def format_date(self, date_str: str) -> date:
+        """
+        Takes a date in mm-dd-yyyy format and returns a date object
+
+        Args:
+            date_str: data in mm-dd-yyyy string format
+        
+        Return
+            a date object
+        """
+        return datetime.strptime(date_str, '%m-%d-%Y').date()
+
+    def execute(self) -> None:
+        """
+        Runs the NWP pipeline according to the configuration file
+        """
+        assert self.config['data_type'] == 'nwp', 'Configuration Error: Expects "nwp" data_type in configuration'
+
+        START_DATE = self.format_date(self.config['start_date'])
+        END_DATE = self.format_date(self.config['end_date'])
+        assert START_DATE <= END_DATE, 'Configuration Error: start date must <= end date'
+
+        TEMPLATE_PATH = f"data/surface/{'YEAR'}/{'MONTH'}/{'DATE'}.zarr.zip"
+        
+        cur_date = START_DATE
+        while cur_date <= END_DATE:
+
+            # download file
+            huggingface_path = TEMPLATE_PATH.replace('YEAR', str(cur_date.year)) \
+                                    .replace('MONTH', str(cur_date.month).zfill(2)) \
+                                    .replace('DATE', str(cur_date.strftime("%Y%m%d")))
+            download_path = self.download(huggingface_path)
+
+            # unzip file
+            unzipped_path = 'unzipped/' + download_path[-30:-4]
+            self.unzip(download_path, unzipped_path)
+            self.teardown(download_path)
+
+            # preprocess data
+                # read in the data using xr, apply crops etc etc and save to disk 
+                # in place of the original data and delete the xarray object in memory
+            self.preprocess(unzipped_path)
+            
+            # upload to GCP
+            self.gcp_upload(unzipped_path, self.config['gcp_bucket'] + '/' + unzipped_path[-30:-4])
+            self.teardown(unzipped_path)
+
+            # increment date
+            cur_date += timedelta(days=1)
 
 
 class SatellitePipeline(GCPPipeline):
@@ -92,103 +191,7 @@ class SatellitePipeline(GCPPipeline):
         self.cleanup()
 
 
-class NWPPipeline(GCPPipeline):
-    def __init__(self, config: str) -> None:
-        super().__init__(config)
-
-
-    def download(self, filepath: str) -> str:
-        """
-        Downloads data from "filepath" location from HugginFace and 
-        returns the location of the downloaded data. If the filepath 
-        is not found, it logs the error in a log file.
-
-        Args:
-            filepath: HuggingFace data location
-        
-        Returns:
-            returns filepath of downloaded data
-        """
-        print(f'\nDownloading: {filepath}...')
-        try:
-            download_path = hf_hub_download(
-                repo_id='openclimatefix/eumetsat-rss',
-                filename=filepath,
-                repo_type=self.config['hf_repo_type'],
-                token=self.config['hf_token'],
-                cache_dir=getcwd() + '/cache'
-            )
-            return download_path
-        except Exception as error:
-            log_file = open(self.config['error_log_path'] + 'error_logs.txt', 'a')
-            log_file.write(str(error))
-            print(error)
-
-        
-    def preprocess(self, filepath: str) -> None:
-        """
-        Preprocesses the zarr file at filepath according to configuration parameters
-
-        Args:
-            filepath: location of zarr path
-        """
-        pass
-
-
-    def format_date(self, date_str: str) -> date:
-        """
-        Takes a date in mm-dd-yyyy format and returns a date object
-
-        Args:
-            date_str: data in mm-dd-yyyy string format
-        
-        Return
-            a date object
-        """
-        return datetime.strptime(date_str, '%m-%d-%Y').date()
-
-
-    def execute(self: str) -> None:
-        """
-        Runs the NWP pipeline according to the configuration file
-        """
-        assert self.config['data_type'] == 'nwp', 'Configuration Error: Expects "nwp" data_type in configuration'
-
-        START_DATE = self.format_date(self.config['start_date'])
-        END_DATE = self.format_date(self.config['end_date'])
-        assert START_DATE <= END_DATE, 'Configuration Error: start date must <= end date'
-
-        TEMPLATE_PATH = f"data/surface/{'YEAR'}/{'MONTH'}/{'DATE'}.zarr.zip"
-        
-        cur_date = START_DATE
-        while cur_date <= END_DATE:
-
-            # download file
-            huggingface_path = TEMPLATE_PATH.replace('YEAR', str(cur_date.year)) \
-                                    .replace('MONTH', str(cur_date.month).zfill(2)) \
-                                    .replace('DATE', str(cur_date.strftime("%Y%m%d")))
-            download_path = self.download(huggingface_path)
-
-            # unzip file
-            unzipped_path = 'unzipped/' + download_path[-30:-4]
-            self.unzip(download_path, unzipped_path)
-            self.teardown(download_path)
-
-            # preprocess data
-                # read in the data using xr, apply crops etc etc and save to disk 
-                # in place of the original data and delete the xarray object in memory
-            self.preprocess(unzipped_path)
-            
-            # upload to GCP
-            self.gcp_upload(unzipped_path, self.config['gcp_bucket'] + '/' + unzipped_path[-30:-4])
-            self.teardown(unzipped_path)
-
-            # increment date
-            cur_date += timedelta(days=1)
-
-
 if __name__ == '__main__':
     config_path = 'nwp_config.json'
     datapipeline = NWPPipeline(config=config_path)
     datapipeline.execute()
-
