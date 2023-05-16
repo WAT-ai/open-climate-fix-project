0d22b750d2fa1799715d99f5f4e6297911c7a93e
Areel Khan <you@example.com>
2023-05-16 01:00:11 -0400

    finished gcp-download script

--- utils/gcp_utils/gcp_utils.py
+++ utils/gcp_utils/gcp_utils.py
@@ -1,7 +1,8 @@
-import os
+import json
 import logging
+import os
+from datetime import timedelta, datetime
 from pathlib import Path
-from datetime import date, timedelta
 
 logging.basicConfig(level=logging.INFO, format='')
 
@@ -21,36 +22,32 @@ class GCPUtils:
         os.system(f'gsutil -m cp -r {remote_path} {destination_path}')
         print('\n')
 
-    def gcp_download_nwp(
-        self, destination_path: str, start_date: date, end_date: date
-    ) -> None:
+    def gcp_download_nwp(self, config_path: str) -> None:
         """
-        Downloads NWP data from start_date to end_date.
+        Downloads NWP data as outlined in the config
         If some date within the given range is not found in the GCP bucket,
         it will be skipped and a corresponding message will be logged
 
         Args:
-            destination_path: path to where data will be downloaded
-            start_date: start_date
-            end_date: end_date
-
-        Returns:
-            None
-
+            config_path: path to JSON config object. It must contain:
+                - destination_path: path to where data will be downloaded
+                - start_date: start_date in dd/mm/yyy
+                - end_date: end_date in dd/mm/yyy
         """
-        if not os.path.isdir(Path(destination_path)):
-            os.mkdir(Path(destination_path))
-
-        cur = start_date
-        while cur <= end_date:
-            remote_path = f"gs://ocf_base_data/nwp/surface/{cur.year}/{cur.month:02}/{cur.year}{cur.month:02}{cur.day:02}.zarr"
-            self.download_dir(remote_path, destination_path)
-            cur += timedelta(1)
+        config: dict = json.load(open(config_path))
+        if not os.path.isdir(Path(config['destination_path'])):
+            os.mkdir(Path(config['destination_path']))
+        
+        start_date = datetime.strptime(config['start_date'], '%d/%m/%Y')
+        end_date =  datetime.strptime(config['end_date'], '%d/%m/%Y')
+        current_date = start_date
+        while current_date <= end_date:
+            remote_path = f"gs://ocf_base_data/nwp/surface/{current_date.year}/{current_date.month:02}/{current_date.year}{current_date.month:02}{current_date.day:02}.zarr"
+            self.download_dir(remote_path, config['destination_path'])
+            current_date += timedelta(1)
 
 
 if __name__ == "__main__":
-    start = date(2022, 1, 21)
-    end = date(2022, 1, 22)
-    path = Path("C:/Users/areel/watai/watai/data/experiment")
+    config_path = './gcp_download_config.json'
     util = GCPUtils()
-    util.gcp_download_nwp(path, start, end)
+    util.gcp_download_nwp(config_path=config_path)
